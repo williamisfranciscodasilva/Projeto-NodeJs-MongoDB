a4332c713253b40f88fc7ae2db6759e00b908a69
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-04-29 18:22:02 -0300

    criando model usuarios, e inserindo dados na coleção

--- aprendendo.js
+++ aprendendo.js
@@ -7,3 +7,41 @@ const mongoose = require('mongoose')
     }).catch((err) => {
         console.log("Houve um erro ao se conectar no mongoDB: "+err)
     })
+
+// Model - Usuários
+const UsuarioSchema= mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String,
+    }
+})
+// Collection
+  mongoose.model('usuarios', UsuarioSchema)
+
+const Usuario = mongoose.model('usuarios')
+  new Usuario({
+      nome: "Ana Paula",
+      sobrenome: "Lins",
+      email: "teste2@email.com",
+      idade: 35,
+      pais: "Brasil"
+  }).save().then(() => {
+      console.log("Usuário criado com sucesso!")
+  }).catch((err) => {
+      console.log("Houve um erro ao registrar o usuário: "+err)
+  })
